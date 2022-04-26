fe04f51103fbd3e77f1b0c7d01aa357ad08f5969
Pranit Yadav <42487480+Pranit46@users.noreply.github.com>
2022-04-26 14:16:17 +0530

    Update Blog.js

--- src/component/Blog.js
+++ src/component/Blog.js
@@ -24,7 +24,7 @@ const Blog = ({ title, description, imageURL, userName, isUser, id }) => {
 
   const deleteRequest = async () => {
     const res = await axios
-      .delete(`http://localhost:4000/api/blogs/delete/${id}`)
+      .delete(`https://socialapp10.herokuapp.com/blogs/delete/${id}`)
       .catch((err) => console.log(err));
     const data = await res.data;
     return data;
